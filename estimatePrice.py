4e9a4a6121c40fecc8ffa866f65d95257abd4cb9
Bastien Mangin <tosba@MacBook-Pro-de-Bastien-2.local>
2025-09-27 00:07:18 +0200

    little secure

--- estimatePrice.py
+++ estimatePrice.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class CarPriceEstimator:
     def __init__(self, theta0: float, theta1: float):
         self.theta0 = theta0
@@ -16,14 +19,29 @@ class CarPriceEstimator:
 
     def get_mileage(self) -> float:
         self.header()
-        mileage = float(input("Enter the car's mileage: "))
-        return mileage
+        while True:
+            try:
+                mileage = float(input("Enter the car's mileage: "))
+                if mileage < 0:
+                    print("Mileage cannot be negative. Try again.")
+                    continue
+                if mileage > 1000000:
+                    print("Wow! It's lot of mileage for a car.")
+                    confirm = input("Are you sure to continue? (y/n): ")
+                    if confirm.lower() in ['n', 'no']:
+                        continue
+                return mileage
+            except ValueError:
+                print("Invalid input. Please enter a numeric value.")
+            except KeyboardInterrupt:
+                print("\nExiting...")
+                sys.exit(0)
+
 
 def main():
     theta0 = 0
     theta1 = 0
     estimator = CarPriceEstimator(theta0, theta1)
-    estimator.header()
     mileage = estimator.get_mileage()
     price = estimator.estimate_price(mileage)
     print(f"Estimated price for a car with {mileage} km: {price:.2f} €")
@@ -31,4 +49,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
